3ff240572880cc9ea8a12e4440c7d1a96b25ac53
Sohottoday <human9062@naver.com>
2020-07-14 10:32:56 +0900

    20200714

---
+++ First/src/variable/VariableTest.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		int age;
+		
+		age = 30;
+		// int age = 30;
+		// int age, count;		같은 자료형일 경우 여러개 선언 가능
+		
+		System.out.println(age);
+
+	}
+
+}
